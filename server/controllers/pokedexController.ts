f62a2b3704e5e83ccd93a268f4cbe4099a62312b
Akshay Jhanji <akshayjhanji@hotmail.com>
2022-05-02 11:45:31 -0700

    Add basic error handling.

--- server/controllers/pokedexController.ts
+++ server/controllers/pokedexController.ts
@@ -1,23 +1,33 @@
 import { Request, Response } from 'express';
+import asyncHandler from 'express-async-handler';
 
-const getPokedex = (req: Request, res: Response) => {
+// GET /api/pokedex
+const getPokedex = asyncHandler(async (req: Request, res: Response) => {
   res.status(200).json({ message: 'Pokedex loaded' });
-};
+});
 
-const initPokedex = (req: Request, res: Response) => {
+// POST /api/pokedex
+const initPokedex = asyncHandler(async (req: Request, res: Response) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error('Please add a text field');
+  }
+
   res.status(200).json({ message: 'Pokedex initialized' });
-};
+});
 
-const updatePokedex = (req: Request, res: Response) => {
+// PUT /api/pokedex/:id
+const updatePokedex = asyncHandler(async (req: Request, res: Response) => {
   res
     .status(200)
     .json({ message: `Pokedex for user ${req.params.id} updated` });
-};
+});
 
-const deletePokedex = (req: Request, res: Response) => {
+// DELETE /api/pokedex/:id
+const deletePokedex = asyncHandler(async (req: Request, res: Response) => {
   res
     .status(200)
     .json({ message: `Pokedex for user ${req.params.id} deleted` });
-};
+});
 
 export { getPokedex, initPokedex, updatePokedex, deletePokedex };
--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,12 @@
-import express, { Application } from 'express';
-import { DotenvConfigOptions } from 'dotenv';
+import express, { Application, json, urlencoded } from 'express';
+import 'dotenv/config';
 import pokedexRoutes from './routes/pokedexRoutes.js';
 
 const port = process.env.PORT || 5000;
 const app: Application = express();
+
+app.use(json());
+app.use(urlencoded({ extended: true }));
 
 app.use('/api/pokedex', pokedexRoutes);
 
