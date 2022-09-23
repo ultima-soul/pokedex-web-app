221e28aff01ee75a69d7d0ee4a9603010944d850
Akshay Jhanji <akshayjhanji@hotmail.com>
2022-09-23 09:44:03 -0700

    Add basic expiring cache using local storage for initial PokeApi request.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,45 +11,76 @@ import {
 const App = () => {
   const [pokedex, setPokedex] = useState<Pokedex>({ entries: [] });
 
-  const getMonBase64Image = (monData: PokemonGeneral) => {
+  const getMonBase64Image = (monData: PokemonGeneral): Promise<string> => {
     return new Promise<string>(async (resolve, reject) => {
       const imgUrl: string =
         monData.sprites.other['official-artwork'].front_default;
-      const imgData = await (await fetch(imgUrl)).blob();
-      const reader = new FileReader();
+      const imgData: Blob = await (await fetch(imgUrl)).blob();
+      const reader: FileReader = new FileReader();
       reader.readAsDataURL(imgData);
       reader.onload = () => resolve(reader.result?.toString() || '');
       reader.onerror = (error) => reject(error);
     });
   };
 
-  const getMonName = async (monData: PokemonGeneral) => {
+  const getMonName = async (monData: PokemonGeneral): Promise<string> => {
     const res = await fetch(monData.species.url);
     const speciesData: PokemonSpecies = await res.json();
     return speciesData.names[8].name;
   };
 
+  const getCacheExpiry = (): string => {
+    const curr: Date = new Date();
+
+    const expiry = curr.setMonth(curr.getMonth() + 1);
+
+    return expiry.toString();
+  };
+
   useEffect(() => {
     const fetchDexEntries = async () => {
-      const res = await fetch(
-        'https://pokeapi.co/api/v2/pokemon?limit=151&offset=0'
-      );
-      const data = await res.json();
+      const pokeApiRes: NamedAPIResource[] = await (async () => {
+        if (localStorage.pokeApiRes) {
+          const expiry = parseInt(localStorage.getItem('expiry') || '');
+
+          if (Date.now() >= expiry) {
+            localStorage.removeItem('pokeApiRes');
+            localStorage.removeItem('expiry');
+          } else {
+            return JSON.parse(localStorage.getItem('pokeApiRes') || '');
+          }
+        }
+
+        const res = await fetch(
+          'https://pokeapi.co/api/v2/pokemon?limit=151&offset=0'
+        );
+        const data = await res.json();
+
+        const pokeApiRes: NamedAPIResource[] = data.results;
+        localStorage.setItem('pokeApiRes', JSON.stringify(pokeApiRes));
+
+        const expiry: string = getCacheExpiry();
+        localStorage.setItem('expiry', expiry);
+
+        return pokeApiRes;
+      })();
 
       const dexEntries: PokedexEntry[] = await Promise.all(
-        data.results.map(async ({ url }: NamedAPIResource) => {
-          const monRes = await fetch(url);
-          const monData: PokemonGeneral = await monRes.json();
+        pokeApiRes.map(
+          async ({ url }: NamedAPIResource): Promise<PokedexEntry> => {
+            const monRes = await fetch(url);
+            const monData: PokemonGeneral = await monRes.json();
 
-          const entry: PokedexEntry = {
-            dexNum: monData.id,
-            name: await getMonName(monData),
-            image: await getMonBase64Image(monData),
-            caught: false,
-          };
+            const entry: PokedexEntry = {
+              dexNum: monData.id,
+              name: await getMonName(monData),
+              image: await getMonBase64Image(monData),
+              caught: false,
+            };
 
-          return entry;
-        })
+            return entry;
+          }
+        )
       );
 
       setPokedex({ entries: dexEntries });
